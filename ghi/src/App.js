099bbce2132c4296751ffddfd5c4242be93f0a8e
Brian <brianvazquez@live.com>
2023-04-03 17:56:25 -0400

    added nav and main page and footer

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,37 +1,30 @@
 import { useEffect, useState } from 'react';
-import Construct from './Construct.js'
 import ErrorNotification from './ErrorNotification';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from './Nav.js';
+import MainPage from './components/mainpage/MainPage.js';
+import Footer from './Footer';
 
 function App() {
-  const [launch_info, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);  
-
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-      console.log('fastapi url: ', url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
-
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, [])
-
 
   return (
-    <div>
-      <ErrorNotification error={error} />
-      <Construct info={launch_info} />
-    </div>
+    <BrowserRouter>
+      <Nav/>
+      <div>
+          {/* <ErrorNotification error={error} /> */}
+        <Routes>
+          <Route path="/">
+            <Route index element={<MainPage/>} />
+
+          </Route>
+
+
+        </Routes>
+      </div>
+      <Footer/>
+    </BrowserRouter>
+
   );
 }
 
