2395bbe2b8221e97fe2db69f952c01c6fc008e91
Diana <dianangaritah@gmail.com>
2023-04-04 11:55:46 -0500

    changes to the main page

--- ghi/src/Footer.js
+++ ghi/src/Footer.js
@@ -1,16 +1,15 @@
 import { Typography } from "@material-tailwind/react";
-
 function Footer() {
     return(
-        <footer className="w-full bg-white p-8">
-            <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-white text-center md:justify-between">
+        <footer className="w-full bg-black p-8">
+            <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-black text-center md:justify-between">
                 <img src="/img/logo-ct-dark.png" alt="logo-ct" className="w-10" />
                 <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
                     <li>
                         <Typography
                             as="a"
                             href="#"
-                            color="blue-gray"
+                            color="white"
                             className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
                         >
                             About Us
@@ -20,7 +19,7 @@ function Footer() {
                         <Typography
                             as="a"
                             href="#"
-                            color="blue-gray"
+                            color="white"
                             className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
                         >
                             Contact Us
@@ -28,12 +27,10 @@ function Footer() {
                     </li>
                 </ul>
             </div>
-            <hr className="my-8 border-blue-gray-50" />
-            <Typography color="blue-gray" className="text-center font-normal">
+            <Typography color="white" className="text-center font-normal">
                 &copy; 2023 Foodies
             </Typography>
         </footer>
     );
 }
-
 export default Footer;
