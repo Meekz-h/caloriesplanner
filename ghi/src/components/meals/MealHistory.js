855e226f4914495f2f205c5615aa5c80ab625b84
Brian Vazquez <brianvazquez@live.com>
2023-04-20 17:53:30 +0000

    Date filter

--- ghi/src/components/meals/MealHistory.js
+++ ghi/src/components/meals/MealHistory.js
@@ -1,10 +1,14 @@
 import { useState } from "react";
+
 import { useGetEntriesQuery } from "../../services/Entries";
 
 
-function MealHistory(categoryFilter) {
+function MealHistory(filter) {
     const { data: entries } = useGetEntriesQuery();
     const [searchMeals, setSearch] = useState([]);
+    let category = filter.categoryFilter
+    let date = filter.dateFilter.toDateString()
+    let total_cal = 0;
 
     return (
         <>
@@ -15,26 +19,33 @@ function MealHistory(categoryFilter) {
                             <th>Name</th>
                             <th>Calories</th>
                             <th>Category</th>
+                            <th>Date</th>
                         </tr>
                     </thead>
                     <tbody>
 
-                    {!entries ? (<></>) : ((categoryFilter.categoryFilter === "all") ? (
-                        entries.map((entry) => {
+                    {!entries ? (<></>) : ((category === "all") ? (
+                            entries.filter((entry) => new Date(entry.date).toDateString() === date).map((entry) => {
+                            total_cal += entry.calories
                     return (
-                        <tr>
+                        <tr key={entry.id}>
                             <td>{entry.name}</td>
                             <td>{entry.calories}</td>
                             <td>{entry.category}</td>
+                            <td>{new Date(entry.date).toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</td>
                         </tr>)})) :
 
-                        (entries.filter(entry => entry.category === categoryFilter.categoryFilter).map((entry) => {
+                            (entries.filter((entry) => new Date(entry.date).toDateString() === date).filter(entry => entry.category === category).map((entry) => {
+                            total_cal += entry.calories
                             return (
-                                <tr>
+                                <tr key={entry.id}>
                                     <td>{entry.name}</td>
                                     <td>{entry.calories}</td>
                                     <td>{entry.category}</td>
-                                </tr>)})))}
+                                    <td>{new Date(entry.date).toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</td>
+                                    </tr>)
+                            })))}
+                        <tr>{total_cal}</tr>
                     </tbody>
                 </table>
             </div>
